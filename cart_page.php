e7aab48663738afd585f5136e1e9dc3c3e793857
Masahiro Fuchigami <mmshr06@gmail.com>
2021-08-24 16:49:48 +0900

    added the file

---
+++ cart_page.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>cart page</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+  </head>
+  <body>
+      <header><?php include 'header.php';?></header>
+      <main>
+        <div class="container-fluid">
+            <div class="card w-50 mx-auto mt-5">
+                <div class="card-header"></div>
+                <div class="card-body">
+                    <!-- input the data how many... -->
+                </div>
+                <div class="card-footer">
+                    <!-- calc with php and shpow and put the button buy now  -->
+                </div>
+            </div>
+        </div>
+      </main>
+      <footer><?php include 'footer.php';?></footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+  </body>
+</html>
