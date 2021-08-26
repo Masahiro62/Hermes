26e12c8e0512312a6a974687a245d1ccedf05766
Masahiro Fuchigami <mmshr06@gmail.com>
2021-08-26 23:24:47 +0900

    added sns

--- showcase.php
+++ showcase.php
@@ -9,7 +9,14 @@
 </head>
 <body>
     <main class="header-main">
+            <div class="sns text-end pt-5 pe-5">
+             <a href="https://twitter.com"><i class="fab fa-twitter-square text-info fs-1"></i></a>
+             <a href="https://facebook.com"><i class="fab fa-facebook-square text-primary fs-1 ms-1 me-1"></i></a>
+             <a href="https://instagram.com"><i class="fab fa-instagram-square text-danger fs-1"></i></a>
+            </div>
+
         <div class="center-box">
+            
             <p>
                 <strong>Enjoy your shopping and have a wonderful time!</strong>
             </p>
